5c3c4e6cb8693f0fd8621269d1b8c664683d6a0c
devc1234 <namdevchavan713@gmail.com>
2024-06-25 14:32:52 +0530

    first commit

---
+++ src/main/java/com/example/Blog_App_Angular_Spring/BlogAppAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Blog_App_Angular_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppAngularSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Blog_App_Angular_Spring/BlogAppAngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blog_App_Angular_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppAngularSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
